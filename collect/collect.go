c942bb61fe1d905c9801033af20766eaa62ca22a
beloslav <velislav1329@gmail.com>
2021-03-04 23:35:59 +0300

    +

--- collect/collect.go
+++ collect/collect.go
@@ -9,20 +9,33 @@ type Collecter interface {
 
 // Characters ...
 type Characters struct {
-	ContinueChar []uint
-	AlphabetChar []uint
+	ContinueChar map[byte]byte
+	AlphabetChar map[byte]byte
 	SpecialChar  []string
+	AllChar      []string
 }
 
 func (c *Characters) Collect(length int) string {
-	return fmt.Sprint("Method interface Collect ", length)
+	for key, _ := range c.AlphabetChar {
+		fmt.Println(string(c.AlphabetChar[key]))
+		if _, exists := c.ContinueChar[key]; exists {
+			continue
+		}
+		c.AllChar = append(c.AllChar, string(c.AlphabetChar[key]))
+	}
+
+	return fmt.Sprint("Method interface Collect ", length, c.AllChar, c.AllChar[51])
 }
 
-func getRange(start uint, end uint) (result []uint) {
-	result = make([]uint, 0, end-start)
+func getRange(start byte, end byte) (result map[byte]byte) {
+	result = make(map[byte]byte, end-start)
+	//result = make([]byte, 0, end-start)
+	// todo: элемент последний end
 	for value := start; value <= end; value++ {
-		result = append(result, value)
+		//result = append(result, value)
+		result[value] = value
 	}
+	fmt.Println(result)
 	return result
 }
 
