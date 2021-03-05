bce33d95a7822f3b81014d26f40676134bc6c21a
beloslav <velislav1329@gmail.com>
2021-03-05 19:24:15 +0300

    промежуточное решение с инициализацией правильных символов

--- collect/collect.go
+++ collect/collect.go
@@ -1,6 +1,9 @@
 package collect
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 // Collecter base interface
 type Collecter interface {
@@ -9,33 +12,29 @@ type Collecter interface {
 
 // Characters ...
 type Characters struct {
-	ContinueChar map[byte]byte
-	AlphabetChar map[byte]byte
+	ContinueChar []int
+	AlphabetChar []int
 	SpecialChar  []string
 	AllChar      []string
 }
 
 func (c *Characters) Collect(length int) string {
-	for key, _ := range c.AlphabetChar {
-		fmt.Println(string(c.AlphabetChar[key]))
-		if _, exists := c.ContinueChar[key]; exists {
+	for _, v := range c.AlphabetChar {
+		n := sort.SearchInts(c.ContinueChar, v)
+		if n < len(c.ContinueChar) && c.ContinueChar[n] == v {
 			continue
 		}
-		c.AllChar = append(c.AllChar, string(c.AlphabetChar[key]))
+		c.AllChar = append(c.AllChar, string(byte(v)))
 	}
-
-	return fmt.Sprint("Method interface Collect ", length, c.AllChar, c.AllChar[51])
+	c.AllChar = append(c.AllChar, c.SpecialChar...)
+	return fmt.Sprint("Method interface Collect ", length, c.AllChar)
 }
 
-func getRange(start byte, end byte) (result map[byte]byte) {
-	result = make(map[byte]byte, end-start)
-	//result = make([]byte, 0, end-start)
-	// todo: элемент последний end
-	for value := start; value <= end; value++ {
-		//result = append(result, value)
-		result[value] = value
+func getRange(start int, end int) (result []int) {
+	result = make([]int, 0, end-start)
+	for value := start; value < end; value++ {
+		result = append(result, value)
 	}
-	fmt.Println(result)
 	return result
 }
 
