267a807b1628b9c2a19f272c6afc15c9acf0eb97
beloslav <velislav1329@gmail.com>
2021-03-06 13:19:05 +0300

    переделал клиентский код

--- main.go
+++ main.go
@@ -5,19 +5,29 @@ import (
 	"github.com/Beloslav13/char-collect/collect"
 )
 
-func getCollect(c collect.Collecter) {
+func getCollect(c collect.Collecter) (string, error) {
 	//switch v := c.(type) {
 	switch c.(type) {
 	case *collect.Characters:
 		characters := c.(*collect.Characters)
-		fmt.Println(characters.Collect(12))
+		chars, err := characters.Collect(12)
+		if err != nil {
+			return "", err
+		}
+		return chars, err
 	default:
 		fmt.Println("Default switch...")
+		return "", nil
 	}
 }
 
 func main() {
 	var c collect.Collecter
 	c = collect.NewCharacters()
-	getCollect(c)
+	chars, err := getCollect(c)
+	if err != nil {
+		fmt.Printf("Error: %s", err)
+	} else {
+		fmt.Println(chars)
+	}
 }
